5cf86bd7309daa4f2ae59fad6c72c082b8746934
krungy <furler711@gmail.com>
2022-01-27 16:40:55 +0900

    📦 chore: 개발환경 초기 셋팅

---
+++ craco.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    // 절대경로 지정
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^\\@components/(.*)$': '<rootDir>/src/components/$1',
+        '^\\@pages/(.*)$': '<rootDir>/src/pages/$1',
+        '^\\@api/(.*)$': '<rootDir>/src/api/$1',
+        '^\\@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^\\@styles/(.*)$': '<rootDir>/src/styles/$1',
+      },
+    },
+  },
+};
