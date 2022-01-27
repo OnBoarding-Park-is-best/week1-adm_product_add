9cfe35de1857cf72590a54a863462218df6dae1f
hyo-choi <hyochoi31@gmail.com>
2022-01-27 23:37:43 +0900

    :recycle: refactor: Button 컴포넌트 리팩토링
    resolves #15

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,4 +1,5 @@
 export { default as Icon } from './Icon';
 export { default as Toggle } from './Toggle';
+export { default as Button } from './Button';
 export { default as ContentContainer } from './ContentContainer';
 export { default as ContentItem } from './ContentItem';
