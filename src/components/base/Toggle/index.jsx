0fa8a32fadd04942f0281c506cc56ee2a93ae73d
krungy <furler711@gmail.com>
2022-01-27 21:35:43 +0900

    ♻️ refactor: styled 위치 변경 & toggle 스타일 수정

--- src/components/base/Toggle/index.jsx
+++ src/components/base/Toggle/index.jsx
@@ -3,48 +3,6 @@ import useToggle from '@hooks/useToggle';
 import { COLORS } from '@utils/constants';
 import PropTypes from 'prop-types';
 
-const ToggleContainer = styled.label`
-  display: inline-block;
-  cursor: pointer;
-  user-select: none;
-`;
-
-const ToggleSwitch = styled.div`
-  width: 54px;
-  height: 18px;
-  padding: 2px;
-  border-radius: 15px;
-  background-color: ${COLORS.grey};
-  transition: background-color 0.2 ease-out;
-  box-sizing: border-box;
-
-  &:after {
-    content: '';
-    position: relative;
-    top: -6px;
-    left: 0;
-    display: block;
-    width: 26px;
-    height: 26px;
-    border-radius: 50%;
-    background-color: ${COLORS.grey_10};
-    transition: left 0.2s ease-out;
-  }
-`;
-
-const ToggleInput = styled.input`
-  display: none;
-
-  &:checked + div {
-    background: ${COLORS.purple_30};
-  }
-
-  &:checked + div:after {
-    left: calc(100% - 26px);
-    background: ${COLORS.purple_50};
-  }
-`;
-
 const Toggle = ({ name, on = false, onChange, ...props }) => {
   const [checked, toggle] = useToggle(on);
 
@@ -77,5 +35,48 @@ Toggle.defaultProps = {
   on: false,
   onChange: () => {},
 };
+
+const ToggleContainer = styled.label`
+  display: inline-block;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const ToggleSwitch = styled.div`
+  width: 54px;
+  height: 18px;
+  padding: 2px;
+  border-radius: 15px;
+  background-color: ${COLORS.grey};
+  transition: background-color 0.2 ease-out;
+  box-sizing: border-box;
+
+  &:after {
+    content: '';
+    position: relative;
+    top: -6px;
+    left: -2px;
+    display: block;
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+    background-color: ${COLORS.grey_10};
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+    transition: left 0.2s ease-out;
+  }
+`;
+
+const ToggleInput = styled.input`
+  display: none;
+
+  &:checked + div {
+    background: ${COLORS.purple_30};
+  }
+
+  &:checked + div:after {
+    left: calc(100% - 24px);
+    background: ${COLORS.purple_50};
+  }
+`;
 
 export default Toggle;
