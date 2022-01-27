7832d688e895959aa8d6c55bcb45df3789a11a24
krungy <furler711@gmail.com>
2022-01-27 17:35:14 +0900

    ✨ feat: 색상 상수 지정

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  purple_90: '#3f1c6f', // 저장하기 뒷배경 및 보더 컬러
+  purple_50: '#2a19bc', // 토글 컬러
+  purple_30: '#aea7e4',
+  yellowgreen: '#dffcd4',
+  grey: '#9f9f9f', // 토글 회색
+  grey_60: '#e3e3e3', // 짙은 회색 배경 (상품 옵션)
+  grey_30: '#fbfbfb', // 옅은 배경 회색
+  grey_10: '#efefef', // 매우 옅은 회색 (이미지 첨부 배경)
+  border: '#f7f6f7', // border 컬러
+  red: '#cd4950', // 삭제버튼
+};
