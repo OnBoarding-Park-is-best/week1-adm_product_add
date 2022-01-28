dcfbddd4df154464ca7cff1e116cc08df685e0c8
krungy <furler711@gmail.com>
2022-01-29 01:31:54 +0900

    ✨ feat: ProductInformationNotice 컴포넌트 구현

---
+++ src/components/domain/ProductInformationNotice/index.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Button, Input } from '@components/base';
+import { COLORS, INFORMATION_NOTICE_CONTENT_LIST } from '@utils/constants';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+
+const ProductInformationNotice = ({ infoIndex, ...props }) => {
+  const [additionalContent, setAdditionalContent] = useState([]);
+
+  const handleAddItem = () => {
+    setAdditionalContent((content) => [
+      ...content,
+      {
+        id: v4(),
+        title: '',
+        content: '',
+      },
+    ]);
+  };
+
+  const handleUpdateItem = (id, status, e) =>
+    status === 'title'
+      ? setAdditionalContent(
+          additionalContent.map((item) =>
+            item.id === id ? { ...item, title: e.target.value } : item,
+          ),
+        )
+      : setAdditionalContent(
+          additionalContent.map((item) =>
+            item.id === id ? { ...item, content: e.target.value } : item,
+          ),
+        );
+
+  const handleDeleteItem = (id) => {
+    setAdditionalContent(additionalContent.filter((item) => id !== item.id));
+  };
+
+  const handleInformationContent = (list) =>
+    list.map(({ title, placeholder }, infoIndex) => (
+      <ItemContainer key={infoIndex}>
+        <Text>{title}</Text>
+        <Input name={title} placeholder={placeholder} padding="13px 8px" />
+      </ItemContainer>
+    ));
+
+  const handleAdditionalInformationContent = (list) =>
+    list.map(({ id, title, content }, index) => (
+      <ItemContainer key={index} id={id}>
+        <Input
+          name={title}
+          placeholder="항목 제목 자유 입력"
+          padding="13px 8px"
+          value={title}
+          onChange={(e) => handleUpdateItem(id, 'title', e)}
+          style={{ width: 258 }}
+        />
+        <Input
+          name={content}
+          placeholder="내용을 입력해주세요."
+          padding="13px 8px"
+          value={content}
+          onChange={(e) => handleUpdateItem(id, 'content', e)}
+        />
+        <Button
+          deleteRed
+          width="120px"
+          style={{ height: 42 }}
+          onClick={() => handleDeleteItem(id)}
+        >
+          삭제
+        </Button>
+      </ItemContainer>
+    ));
+
+  return (
+    <ProductInformationNoticeContainer {...props}>
+      <TitleContainer>
+        <Text componentTitle>정보고시 {infoIndex}</Text>
+        <Button deleteGrey>삭제</Button>
+      </TitleContainer>
+      <ContentContainer>
+        {handleInformationContent(INFORMATION_NOTICE_CONTENT_LIST)}
+        {handleAdditionalInformationContent(additionalContent)}
+      </ContentContainer>
+      <Button plusIcon onClick={() => handleAddItem()}>
+        항목 추가
+      </Button>
+    </ProductInformationNoticeContainer>
+  );
+};
+
+ProductInformationNotice.propTypes = {
+  infoIndex: PropTypes.number,
+};
+
+ProductInformationNotice.defaultProps = {
+  infoIndex: 1,
+};
+
+const ProductInformationNoticeContainer = styled.div`
+  padding: 32px;
+  border: 1px solid ${COLORS.border};
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin-bottom: 18px;
+  > Button {
+    position: absolute;
+    right: 0;
+  }
+`;
+
+const Text = styled.div`
+  font-size: ${({ componentTitle }) => (componentTitle ? '18px' : '16px')};
+  width: 240px;
+  font-weight: ${({ componentTitle }) => (componentTitle ? 600 : 400)};
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 18px;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 18px;
+  height: 62px;
+`;
+
+export default ProductInformationNotice;
--- src/components/domain/index.js
+++ src/components/domain/index.js
@@ -1,3 +1,4 @@
 export { default as ImageUploadContainer } from './ImageUploadContainer';
 export { default as Others } from './Others';
 export { default as Benefit } from './Benefit';
+export { default as ProductInformationNotice } from './ProductInformationNotice';
