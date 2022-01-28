5ef90a5d8783286dfca9c4e404d9c0ccd4171185
Junghoon-P <qkr0454@naver.com>
2022-01-28 16:21:33 +0900

    :recycle: refactor: checkBox name 추가

--- src/components/base/CheckBox/index.jsx
+++ src/components/base/CheckBox/index.jsx
@@ -4,23 +4,34 @@ import { Icon } from '@components/base';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const CheckBox = ({id, name, onChange, ...props}) => {
-  const [ isChecked, toggle ] = useToggle(false);
+const CheckBox = ({ id, name, onChange, ...props }) => {
+  const [isChecked, toggle] = useToggle(false);
 
   const checkHandler = (e) => {
     toggle();
     onChange && onChange(e);
-  }
+  };
 
   return (
     <CheckBoxLabel htmlFor={id}>
-      <CheckBoxInput id={id} type="checkbox" checked={isChecked} onChange={checkHandler}/>
+      <CheckBoxInput
+        id={id}
+        type="checkbox"
+        checked={isChecked}
+        onChange={checkHandler}
+        name={name}
+      />
       <StyledCheckBox checked={isChecked}>
-      <Icon icon="akar-icons:check" color="#fff" height={20} style={iconStyle}/>
+        <Icon
+          icon="akar-icons:check"
+          color="#fff"
+          height={20}
+          style={iconStyle}
+        />
       </StyledCheckBox>
-      { name }
+      {name}
     </CheckBoxLabel>
-  )
+  );
 };
 
 CheckBox.propTypes = {
@@ -35,16 +46,16 @@ const CheckBoxLabel = styled.label`
 `;
 
 const CheckBoxInput = styled.input`
-  position: absolute; 
+  position: absolute;
   width: 1px;
-  height: 1px; 
-  margin: -1px; 
-  padding: 0; 
-  border: 0; 
-  clip: rect(0 0 0 0); 
-  clippath: inset(50%); 
-  overflow: hidden; 
-  white-space: nowrap; 
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+  clip: rect(0 0 0 0);
+  clippath: inset(50%);
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 const StyledCheckBox = styled.div`
@@ -53,12 +64,13 @@ const StyledCheckBox = styled.div`
   height: 1.5rem;
   margin-right: 10px;
   border: solid 0.1rem #dddddd;
-  background: ${props => props.checked ? COLORS.purple_90 : 'white'}; 
-  border-radius: 0.4rem; transition: all 150ms;
+  background: ${(props) => (props.checked ? COLORS.purple_90 : 'white')};
+  border-radius: 0.4rem;
+  transition: all 150ms;
 `;
 
-const iconStyle = ({isActive}) => ({
-  visibility: isActive ? 'visiblie' : 'hidden'
-})
+const iconStyle = ({ isActive }) => ({
+  visibility: isActive ? 'visiblie' : 'hidden',
+});
 
 export default CheckBox;
--- src/components/domain/ProductBaseInfo/index.jsx
+++ src/components/domain/ProductBaseInfo/index.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageUploadContainer from '../ImageUploadContainer';
 import ContentContainer from '../../base/ContentContainer';
 import ContentItem from '../../base/ContentItem';
 import CheckBox from '../../base/CheckBox';
 import styled from 'styled-components';
-import { FILTER_LIST } from '@utils/constants';
+import { FILTER_LIST, COLORS, BORDER_STYLE } from '@utils/constants';
+import useToggle from '@hooks/useToggle';
 
 const ProductBaseInfo = () => {
+  const [categoryChk, setCategoryChk] = useState([]);
+
+  const checkedHandler = (e) => {
+    console.log('e.target.checked', e.target.checked);
+    console.log('e.target.name', e.target.name);
+  };
+
   return (
     <div>
       <ContentContainer title={'상품 기본 정보'}>
         <ContentItem title={'카테고리'} required>
           <CategoryWrap>
             <div>
-              <CheckBox name={'abc'}></CheckBox>
+              {FILTER_LIST.map((list) => {
+                return <CheckBox onChange={checkedHandler} name={list.title} />;
+              })}
             </div>
             <aside>aa</aside>
           </CategoryWrap>
@@ -27,11 +37,19 @@ const ProductBaseInfo = () => {
 const CategoryWrap = styled.article`
   display: flex;
   align-items: flex-start;
+  padding: 1rem;
 
-  div {
+  & > div {
+    flex: 1;
+    padding: 1rem;
+    border: ${BORDER_STYLE};
   }
 
-  aside {
+  & > aside {
+    flex: 1;
+
+    padding: 1rem;
+    border: ${BORDER_STYLE};
   }
 `;
 
